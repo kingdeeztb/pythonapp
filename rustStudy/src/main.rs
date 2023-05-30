ffda0f4627080fd902e4a6b908ea35b733aabcc5
kingdeeztb@163.com <kingdeeztb@163.com>
2023-05-30 15:43:23 +0800

    wenjian

--- rustStudy/src/main.rs
+++ rustStudy/src/main.rs
@@ -1,8 +1,12 @@
+fn add(a:i32,b:i32)->i32{
+    return a+b;
+}
+
 fn main() {
     let mut number = 1;
     while number != 4 {
         println!("{}", number);
         number += 1;
     }
-    println!("EXIT");
+    println!("EXIT{}",add(7,8));
 }
